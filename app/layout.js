ceafcb198ff965007181983887521f53d4ee2644
MelAlejandrino <alejandrinomel02@gmail.com>
2023-12-12 10:25:56 +0800

    insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from "@components/Navbar";
 import "./globals.css";
 import { Poppins } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next';
  
 const inter = Poppins({ weight: '400', subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
         <main>{children}</main>
         <Navbar />
       </body>
