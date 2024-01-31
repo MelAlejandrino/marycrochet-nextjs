c9ccf61a9093c1ce3400692915a72306eb5d2bf2
MelAlejandrino <alejandrinomel02@gmail.com>
2024-01-31 16:30:21 +0800

    removing localstorage

--- app/api.js
+++ app/api.js
@@ -1,22 +1,23 @@
+const cache = {};
 
 export async function fetchProducts(url) {
-    const cachedData = localStorage.getItem(url);
+    const cachedData = cache[url];
     if (cachedData) {
-      return JSON.parse(cachedData);
+        return cachedData;
     }
-  
+
     const response = await fetch(url);
     const responseData = await response.json();
-  
+
     if (response.status !== 200) {
-      console.error('Error fetching data:', responseData.error.message);
-      return { data: [], nextPage: null };
+        console.error('Error fetching data:', responseData.error.message);
+        return { data: [], nextPage: null };
     } else {
-      const data = responseData.data.sort(() => Math.random() - 0.5);
-      const nextPage = responseData.paging && responseData.paging.next;
-  
-      localStorage.setItem(url, JSON.stringify({ data, nextPage }));
-  
-      return { data, nextPage };
+        const data = responseData.data.sort(() => Math.random() - 0.5);
+        const nextPage = responseData.paging && responseData.paging.next;
+
+        cache[url] = { data, nextPage };
+
+        return { data, nextPage };
     }
-  }
+}
